085283d713f82ecb151e100830b03f91714f3f10
Greg McKeon <grm19@duke.edu>
2016-02-02 15:42:04 -0500

    moved login to navbar.  Login now is actually a login, rather than a create user.  added toastr for login failures

--- resourceful/client/templates/login/login.js
+++ resourceful/client/templates/login/login.js
@@ -9,3 +9,31 @@ Template.login.helpers({
     }
 });
 
+Template.login.events({
+            'submit #login-form': function (event) {
+                event.preventDefault();
+                // Auto-encrypts password, logs user in if successful
+                var emailVar = event.target.email.value;
+                var passVar = event.target.password.value;
+                Meteor.loginWithPassword(emailVar, passVar, function (err) {
+                            console.log("was called");
+                            if (Meteor.user()) {
+                                console.log("logged");
+                                Router.go('/');
+                            } else {
+                                console.log(err.reason);
+                                toastr.options = {
+                                    "positionClass": "toast-bottom-right",
+                                }
+                                    toastr.error(err.reason);
+
+                                }
+                            });
+                    },
+
+                    'click #logout-button': function (event) {
+                        console.log("logging out");
+                        event.preventDefault();
+                        Meteor.logout();
+                    }
+            });
