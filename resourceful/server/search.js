c02c93b782b4aaa028ad6f3202c72be7bd7f9a5d
Greg McKeon <grm19@duke.edu>
2016-02-08 15:05:22 -0500

    search does not work for multiple tags/not tags, need to fix regex

---
+++ resourceful/server/search.js
@@ -0,0 +1,20 @@
+SearchSource.defineSource('tags', function(searchText, options) {
+  var options = {sort: {isoScore: -1}, limit: 20};
+
+  if(searchText) {
+    var regExp = buildRegExp(searchText);
+    var selector = {tags: {$in: [regExp]}};
+    return Resources.find(selector, options).fetch();
+  } else {
+    return Resources.find({}, options).fetch();
+  }
+});
+
+function buildRegExp(searchText) {
+  var words = searchText.trim().split(/[\,\-\:]+/);
+  var exps = _.map(words, function(word) {
+    return "(?=.*" + word + ")";
+  });
+  var fullExp = exps.join('') + ".+";
+  return new RegExp(fullExp, "i");
+}
