4cd07b840b044237ab9da13625bee985b839c93f
Inan Tainwala <it15@duke.edu>
2016-02-07 21:23:18 -0500

    Added some code to be able to add more admin users. Problem with adding more fields to the user object. Needs some fixing.

--- resourceful/client/templates/addition/users.js
+++ resourceful/client/templates/addition/users.js
@@ -1,17 +1,21 @@
 Template.users.events({
-    'submit #create-user-form': function (event) {
+    'submit #create-user-form': function (event, template) {
         event.preventDefault();
         // Auto-encrypts password, logs user in if successful
         var emailVar = event.target.email.value;
         var passVar = event.target.password.value;
         var nameVar = event.target.name.value;
+        var adminVar = template.find('input:radio[name=isAdmin]:checked').value;
+        console.log(adminVar);
         Accounts.createUser({
             email: emailVar,
             password: passVar,
-            name: nameVar
+            name: nameVar,
+            profile: adminVar
         }, function (err) {
             if (!err) {
                 toastr.success("User "+ emailVar + " added!");
+                template.find("form").reset();
             } else {
                 console.log(err.reason);
                 toastr.error(err.reason);
--- resourceful/server/user-config.js
+++ resourceful/server/user-config.js
@@ -1,12 +1,16 @@
 Accounts.onCreateUser(function (options, user) {
     var userId = user._id = Random.id();
     console.log(Meteor.users.find().count());
-    if (Meteor.users.find().count() == 0) {
-    var handle = Meteor.users.find({_id: userId}, {fields: {_id: 1}}).observe({
-        added: function () {
-            Roles.setUserRoles(userId, 'admin');
-            handle.stop();
-            handle = null;
+    console.log('Creating New User');
+    // console.log(Meteor.user().roles.indexOf("admin", 0));
+    console.log(user);
+    if (Meteor.users.find().count() == 0 || ( Meteor.user().roles.indexOf("admin", 0) > -1 && user.isAdmin == "true") ) {
+        console.log('Creating Admin User');
+        var handle = Meteor.users.find({_id: userId}, {fields: {_id: 1}}).observe({
+            added: function () {
+                Roles.setUserRoles(userId, 'admin');
+                handle.stop();
+                handle = null;
         }
     });
         
@@ -18,7 +22,3 @@ Accounts.onCreateUser(function (options, user) {
     }
     return user;
 });
-
-
-
-
